6adeb07d90e758ddea2576cd578554c26fb8dc9e
Roger Orr <rogero@howzatt.demon.co.uk>
2019-01-14 21:49:44 +0000

    First version published as P1392R0

--- functions_and_function_template_examples/function_pointers.cpp
+++ functions_and_function_template_examples/function_pointers.cpp
@@ -1,14 +1,24 @@
 struct X {};
 
 void foo(X, X);
+X make_foo();
 
 template<typename T>
 void bar(T, T);
 
+template<typename T>
+T make_bar();
+
 int main()
 {
   auto foo_ptr = &foo; // Ok
-  void (*barX_ptr)(X, X) = &bar;
+  auto make_foo_ptr = &make_foo; // Ok
 
 //  auto bar_ptr = &bar; // Error
+//  auto make_bar_ptr = &make_bar; // Error
+
+  auto bar_ptr1 = &bar<X>;
+  auto make_bar_ptr1 = &make_bar<X>;
+  
+  void (*bar_ptr2)(X, X) = &bar; // OK, template arguments deduced
 }
